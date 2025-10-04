2ee7551bf8d0223b9aad3bf1cfa12ba513af2ae5
fabianblanco20 <fab.blancosolis@gmail.com>
2025-10-04 10:16:22 -0600

    Agregar captura de datos de usuario con Scanner

--- ProyectoColaborativo/src/proyectocolaborativo/ProyectoColaborativo.java
+++ ProyectoColaborativo/src/proyectocolaborativo/ProyectoColaborativo.java
@@ -4,17 +4,55 @@
  */
 package proyectocolaborativo;
 
+import java.util.Scanner;
 /**
  *
  * @author Fresh
  */
 public class ProyectoColaborativo {
 
-    /**
-     * @param args the command line arguments
-     */
+ 
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        
+        Scanner entrada = new Scanner (System.in);
+        
+        
+        String nombre, apellido, email, telefono, direccion;
+       
+        
+        System.out.println("*Captura de los datos del usuario*");
+        
+        System.out.println("Ingrese su nombre: ");
+        nombre = entrada.nextLine();
+        
+        System.out.println("Ingrese su apellido: ");
+        apellido = entrada.nextLine();
+        
+        System.out.println("Ingrese su correo electronico: ");
+        email = entrada.nextLine();
+        
+        System.out.println("Ingrese su numero de telefono: ");
+        telefono = entrada.nextLine();
+        
+        System.out.println("Ingrese su direccion: ");
+        direccion = entrada.nextLine();
+    
+    
+        System.out.println("*Resumen de la captura de datos*");
+        System.out.println("Nombre: "+ nombre + " " + apellido);
+        System.out.println("Correo electronico: "+ email);
+        System.out.println("Telefono: "+ telefono);
+        System.out.println("Direccion: "+ direccion);
+        
+        
+        
+        
+        
+        
+        
+        
     }
+    
     
 }
